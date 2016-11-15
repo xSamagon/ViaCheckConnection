283fc1bbc5628b5e4a0d055120e2662b771c39b2
Nicolas-PC\Nicolas <x9nico2@outlook.fr>
2016-11-15 17:28:15 +0100

    1.0

---
+++ ViaCheckConnection/src/fr/x9nico/viacheckconnection/Main.java
@@ -0,0 +1,40 @@
+package fr.x9nico.viacheckconnection;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import us.myles.ViaVersion.api.Via;
+import us.myles.ViaVersion.api.protocol.ProtocolVersion;
+
+public class Main extends JavaPlugin implements Listener{
+	
+	@Override
+	public void onEnable() {
+		getServer().getPluginManager().registerEvents(this, this);
+	}
+	
+	@SuppressWarnings("unchecked")
+	@EventHandler
+	public void join(PlayerJoinEvent e){
+		Player p = e.getPlayer();
+		if(Via.getAPI().getPlayerVersion(p) == ProtocolVersion.v1_8.getId()){
+			p.sendMessage("§6[ViaCheck] §rYou are connected from the server with a §b1.8§r's version");
+		} else if(Via.getAPI().getPlayerVersion(p) == ProtocolVersion.v1_9.getId()){
+			p.sendMessage("§6[ViaCheck] §rYou are connected from the server with a §b1.9§r's version");
+		} else if(Via.getAPI().getPlayerVersion(p) == ProtocolVersion.v1_9_1.getId()){
+			p.sendMessage("§6[ViaCheck] §rYou are connected from the server with a §b1.9§r's version");
+		} else if(Via.getAPI().getPlayerVersion(p) == ProtocolVersion.v1_9_2.getId()){
+			p.sendMessage("§6[ViaCheck] §rYou are connected from the server with a §b1.9§r's version");
+		} else if(Via.getAPI().getPlayerVersion(p) == ProtocolVersion.v1_9_3.getId()){
+			p.sendMessage("§6[ViaCheck] §rYou are connected from the server with a §b1.9§r's version");
+		} else if(Via.getAPI().getPlayerVersion(p) == ProtocolVersion.v1_10.getId()){
+			p.sendMessage("§6[ViaCheck] §rYou are connected from the server with a §b1.10§r's version");
+		} else if(Via.getAPI().getPlayerVersion(p) == ProtocolVersion.v1_11.getId()){
+			p.sendMessage("§6[ViaCheck] §rYou are connected from the server with a §b1.11§r's version");
+		}
+	}
+
+}
