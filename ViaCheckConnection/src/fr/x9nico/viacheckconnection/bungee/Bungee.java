6578123305d01f90f2070be30415ea18b750d4c9
Nicolas-PC\Nicolas <x9nico2@outlook.fr>
2016-11-23 19:26:03 +0100

    BungeeCord's support is now available.

--- ViaCheckConnection/src/fr/x9nico/viacheckconnection/bungee/Bungee.java
+++ ViaCheckConnection/src/fr/x9nico/viacheckconnection/bungee/Bungee.java
@@ -1,13 +1,41 @@
 package fr.x9nico.viacheckconnection.bungee;
 
+import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.md_5.bungee.api.event.ServerConnectedEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.api.plugin.Plugin;
+import net.md_5.bungee.event.EventHandler;
+import us.myles.ViaVersion.api.Via;
+import us.myles.ViaVersion.api.protocol.ProtocolVersion;
 
 public class Bungee extends Plugin implements Listener{
 	
 	@Override
 	public void onEnable() {
 		System.out.println("[ViaCheckConnection] WARNING! It is not a BungeeCord's plugin ! please set this plugin on your spigot's plugins folder.");
+		getProxy().getPluginManager().registerListener(this, this);
+	}
+	
+	@SuppressWarnings("unchecked")
+	@EventHandler
+	public void join(ServerConnectedEvent e){
+		ProxiedPlayer p = e.getPlayer();
+		if(Via.getAPI().getPlayerVersion(p) == ProtocolVersion.v1_8.getId()){
+			p.sendMessage(new TextComponent("§6[ViaCheck] §rYou are connected from the server with a §b1.8§r's version"));
+		} else if(Via.getAPI().getPlayerVersion(p) == ProtocolVersion.v1_9.getId()){
+			p.sendMessage(new TextComponent("§6[ViaCheck] §rYou are connected from the server with a §b1.9§r's version"));
+		} else if(Via.getAPI().getPlayerVersion(p) == ProtocolVersion.v1_9_1.getId()){
+			p.sendMessage(new TextComponent("§6[ViaCheck] §rYou are connected from the server with a §b1.9§r's version"));
+		} else if(Via.getAPI().getPlayerVersion(p) == ProtocolVersion.v1_9_2.getId()){
+			p.sendMessage(new TextComponent("§6[ViaCheck] §rYou are connected from the server with a §b1.9§r's version"));
+		} else if(Via.getAPI().getPlayerVersion(p) == ProtocolVersion.v1_9_3.getId()){
+			p.sendMessage(new TextComponent("§6[ViaCheck] §rYou are connected from the server with a §b1.9§r's version"));
+		} else if(Via.getAPI().getPlayerVersion(p) == ProtocolVersion.v1_10.getId()){
+			p.sendMessage(new TextComponent("§6[ViaCheck] §rYou are connected from the server with a §b1.10§r's version"));
+		} else if(Via.getAPI().getPlayerVersion(p) == ProtocolVersion.v1_11.getId()){
+			p.sendMessage(new TextComponent("§6[ViaCheck] §rYou are connected from the server with a §b1.11§r's version"));
+		}
 	}
 
 }
