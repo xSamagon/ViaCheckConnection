96165ace859204093bc11cf3efaac0507e5b6183
Nico <x9nico2@outlook.fr>
2017-02-19 19:35:21 +0100

    fix

--- ViaCheckConnection/src/fr/x9nico/viacheckconnection/Main.java
+++ ViaCheckConnection/src/fr/x9nico/viacheckconnection/Main.java
@@ -48,7 +48,7 @@ public class Main extends JavaPlugin implements Listener{
 		} else if(Via.getAPI().getPlayerVersion(p) == ProtocolVersion.v1_11_1.getId()){
 			p.sendMessage("§6[ViaCheck] §rYou are connected from the server with a §b1.11§r's version");
 		}  else if(Via.getAPI().getPlayerVersion(p) == ProtocolVersion.snapshot.getId()){
-			p.sendMessage("§6[ViaCheck] §rYou are connected from the server with a §b1.12§r's version");
+			p.sendMessage("§6[ViaCheck] §rYou are connected from the server with an §b1.12§r snapshot version");
 		} else if(Via.getAPI().getPlayerVersion(p) == ProtocolVersion.unknown.getId()){
 			p.sendMessage("§6[ViaCheck] §cERROR! We don't find your minecraft's version.Please report this bug !");
 		}
