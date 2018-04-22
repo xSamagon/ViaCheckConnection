7b86ffc7c4acba3b0722c36d4683b05a1c7cf10d
Nicolas RAYNAUD <x9nico2@outlook.fr>
2018-04-22 16:40:21 +0200

    Fixed something

--- ViaCheckConnection/bukkit/fr/x9nico/viacheckconnection/Main.java
+++ ViaCheckConnection/bukkit/fr/x9nico/viacheckconnection/Main.java
@@ -45,19 +45,32 @@ public class Main extends JavaPlugin implements Listener{
 		if(Main.isProtocolSupport()){	
 			
 			//I'm not sure if there was a need to get the server version. As long as the player has a version, this will send them the "message:" option in the config.
-			if(Via.getAPI().getPlayerVersion(p) == ProtocolVersion.v1_8.getID() || Via.getAPI().getPlayerVersion(p) == ProtocolVersion.v1_9_0.getID() || Via.getAPI().getPlayerVersion(p) == ProtocolVersion.v1_9_1.getID() || Via.getAPI().getPlayerVersion(p) == ProtocolVersion.v1_9_2.getID() || Via.getAPI().getPlayerVersion(p) == ProtocolVersion.v1_9_3 || Via.getAPI().getPlayerVersion(p) == ProtocolVersion.v1_10.getID() || Via.getAPI().getPlayerVersion(p) == ProtocolVersion.v1_11.getID() || Via.getAPI().getPlayerVersion(p) == ProtocolVersion.v1_11_1.getID() || Via.getAPI().getPlayerVersion(p) == ProtocolVersion.v1_12.getID() || Via.getAPI().getPlayerVersion(p) == ProtocolVersion.v1_12_1.getID() || Via.getAPI().getPlayerVersion(p) == ProtocolVersion.v1_12_2.getID() || Via.getAPI().getPlayerVersion(p) == ProtocolVersion.v1_13.getID()) {
+			if(Via.getAPI().getPlayerVersion(p) == ProtocolVersion.v1_7_1.getId() || Via.getAPI().getPlayerVersion(p) == ProtocolVersion.v1_7_6.getId() || Via.getAPI().getPlayerVersion(p) == ProtocolVersion.v1_8.getId() || Via.getAPI().getPlayerVersion(p) == ProtocolVersion.v1_9_0.getId() || Via.getAPI().getPlayerVersion(p) == ProtocolVersion.v1_9_1.getId() || Via.getAPI().getPlayerVersion(p) == ProtocolVersion.v1_9_2.getId() || Via.getAPI().getPlayerVersion(p) == ProtocolVersion.v1_9_3.getId() || Via.getAPI().getPlayerVersion(p) == ProtocolVersion.v1_10.getId() || Via.getAPI().getPlayerVersion(p) == ProtocolVersion.v1_11.getId() || Via.getAPI().getPlayerVersion(p) == ProtocolVersion.v1_11_1.getId() || Via.getAPI().getPlayerVersion(p) == ProtocolVersion.v1_12.getId() || Via.getAPI().getPlayerVersion(p) == ProtocolVersion.v1_12_1.getId() || Via.getAPI().getPlayerVersion(p) == ProtocolVersion.v1_12_2.getId() || Via.getAPI().getPlayerVersion(p) == ProtocolVersion.v1_13.getId()) {
 				boolean pv = true;
 				if(pv == true){
 				  p.sendMessage(getConfig().getString("message").replace("&", "§"));
-				  getConfig().replaceAll("%version%", Via.getAPI().getPlayerVersion(p));
+				  getConfig().replace("%version%", Via.getAPI().getPlayerVersion(p));
 				}
 			else if(Via.getAPI().getPlayerVersion(p) == ProtocolVersion.unknown.getId()){
--					p.sendMessage(getConfig().getString("unknown_version").replace("&", "§"));
+					p.sendMessage(getConfig().getString("unknown_version").replace("&", "§"));
 			}
 		} else {
-			p.sendMessage(ChatColor.RED + "ERROR");
+			p.sendMessage(getConfig().getString("unknown_version").replace("&", "§"));
 		}
-	}
+	} else {
+			if(Via.getAPI().getPlayerVersion(p) == ProtocolVersion.v1_8.getId() || Via.getAPI().getPlayerVersion(p) == ProtocolVersion.v1_9_0.getId() || Via.getAPI().getPlayerVersion(p) == ProtocolVersion.v1_9_1.getId() || Via.getAPI().getPlayerVersion(p) == ProtocolVersion.v1_9_2.getId() || Via.getAPI().getPlayerVersion(p) == ProtocolVersion.v1_9_3.getId() || Via.getAPI().getPlayerVersion(p) == ProtocolVersion.v1_10.getId() || Via.getAPI().getPlayerVersion(p) == ProtocolVersion.v1_11.getId() || Via.getAPI().getPlayerVersion(p) == ProtocolVersion.v1_11_1.getId() || Via.getAPI().getPlayerVersion(p) == ProtocolVersion.v1_12.getId() || Via.getAPI().getPlayerVersion(p) == ProtocolVersion.v1_12_1.getId() || Via.getAPI().getPlayerVersion(p) == ProtocolVersion.v1_12_2.getId() || Via.getAPI().getPlayerVersion(p) == ProtocolVersion.v1_13.getId()) {
+				boolean pv = true;
+				if(pv == true){
+					p.sendMessage(getConfig().getString("message").replace("&", "§"));
+					getConfig().replace("%version%", Via.getAPI().getPlayerVersion(p));
+				}
+				else if(Via.getAPI().getPlayerVersion(p) == ProtocolVersion.unknown.getId()){
+					p.sendMessage(getConfig().getString("unknown_version").replace("&", "§"));
+				}
+			} else {
+				p.sendMessage(getConfig().getString("unknown_version").replace("&", "§"));
+			}
+		}
 	
 	public static boolean isProtocolSupport(){
 		return true;
