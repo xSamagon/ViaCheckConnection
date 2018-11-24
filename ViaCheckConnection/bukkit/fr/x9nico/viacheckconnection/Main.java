ac428bc83e053aa552a56a0823a10a715d9fdb73
xSamagon <31865681+xSamagon@users.noreply.github.com>
2018-11-24 15:04:51 +0100

    Update Main.java
    Changed tabs back to 4 spaces

--- ViaCheckConnection/bukkit/fr/x9nico/viacheckconnection/Main.java
+++ ViaCheckConnection/bukkit/fr/x9nico/viacheckconnection/Main.java
@@ -14,55 +14,55 @@ import us.myles.ViaVersion.api.protocol.ProtocolVersion;
 
 public class Main extends JavaPlugin implements Listener{
 
-	FileConfiguration config;
-	static Main instance;
-	public static Main getInstance(){return instance;}
+    FileConfiguration config;
+    static Main instance;
+    public static Main getInstance(){return instance;}
 
-	@Override
-	public void onEnable() {
-		getLogger().info("You are using the plugin on a Spigot server");
-		getLogger().info("Plugin version: "+getDescription().getVersion());
-		getServer().getPluginManager().registerEvents(this, this);
-		getConfig().options().copyDefaults(true);
-		saveConfig();
-		getCommand("viacheck").setExecutor(new ViaCheckReloadCommand());
-		instance = this;
-		boolean isUpdate = Updater.checkForUpdate(this, 31805, false);
-		if(isUpdate){
-			getLogger().info("We found a plugin update! Downloading it...");
-			boolean success = Updater.download(this, 31805);
-			if(success){
-				getLogger().info("The plugin has been updated, please restart the server.");
-			} else {
-				getLogger().info("The plugin couldn't be updated. Either Spiget is down or you're using a dev version.");
-			}
-		}
+    @Override
+    public void onEnable() {
+        getLogger().info("You are using the plugin on a Spigot server");
+        getLogger().info("Plugin version: "+getDescription().getVersion());
+        getServer().getPluginManager().registerEvents(this, this);
+        getConfig().options().copyDefaults(true);
+        saveConfig();
+        getCommand("viacheck").setExecutor(new ViaCheckReloadCommand());
+        instance = this;
+        boolean isUpdate = Updater.checkForUpdate(this, 31805, false);
+        if(isUpdate){
+            getLogger().info("We found a plugin update! Downloading it...");
+            boolean success = Updater.download(this, 31805);
+            if(success){
+                getLogger().info("The plugin has been updated, please restart the server.");
+            } else {
+                getLogger().info("The plugin couldn't be updated. Either Spiget is down or you're using a dev version.");
+            }
+        }
 
-		if(Bukkit.getVersion().contains("1.12") || Bukkit.getVersion().contains("1.11") || Bukkit.getVersion().contains("1.10") || Bukkit.getVersion().contains("1.9") || Bukkit.getVersion().contains("1.13")) {
-			getServer().getPluginManager().enablePlugin(this);
-		}
-		new Metrics(this);
-	}
+        if(Bukkit.getVersion().contains("1.12") || Bukkit.getVersion().contains("1.11") || Bukkit.getVersion().contains("1.10") || Bukkit.getVersion().contains("1.9") || Bukkit.getVersion().contains("1.13")) {
+            getServer().getPluginManager().enablePlugin(this);
+        }
+        new Metrics(this);
+    }
 
-	@SuppressWarnings("unchecked")
-	@EventHandler
-	public void join(PlayerJoinEvent e){
-		Player p = e.getPlayer();
+    @SuppressWarnings("unchecked")
+    @EventHandler
+    public void join(PlayerJoinEvent e){
+        Player p = e.getPlayer();
 
-		int id = Via.getAPI().getPlayerVersion(p);
-		if (id == -1 || ProtocolVersion.isRegistered(id) == false)
+        int id = Via.getAPI().getPlayerVersion(p);
+        if (id == -1 || ProtocolVersion.isRegistered(id) == false)
         {
             p.sendMessage(getConfig().getString("unknown_version").replace("&", "§"));
             return;
         }
 
         //Srsly its easier to change your configfile, but anyway: Like this it should support your configfile almost completly
-		String name = "v"+ProtocolVersion.getProtocol(id).getName().split("/")[0].split("-")[0].replace(".x", "").replace(".", "_");
-		p.sendMessage(getConfig().getString(name).replace("&", "§"));
+        String name = "v"+ProtocolVersion.getProtocol(id).getName().split("/")[0].split("-")[0].replace(".x", "").replace(".", "_");
+        p.sendMessage(getConfig().getString(name).replace("&", "§"));
 
-		//Alternative way ( way simpler, but w/o config)
+        //Alternative way ( way simpler, but w/o config)
 
         p.sendMessage("Your are using Minecraft version: "+ProtocolVersion.getProtocol(id).getName());
-	}
+    }
 
 }
